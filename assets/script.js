cabab315a6bb59e6052b8597624443cee8d7db34
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-04-12 09:45:08 +0200

    Esercizio corretto e migliorato...

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,12 @@
 //inserimento ciclo
 for(i = 1; i < 101; i++){
   var numeri = i;
-  //ricchiesta multipli di 3
-  if(i % 3 == 0){
+  if(i % 3 === 0 && i % 5 === 0){    //richiesta di entrambi multipli
     console.log('Fizz');
-  }else if(i % 5 == 0){   //ricchiesta multipli di 5
+  }else if(i % 5 === 0){   //ricchiesta multipli di 5
     console.log('Buzz');
-  }else if(i % 3 == 0 && i % 5 == 0){   //richiesta di entrambi multipli
-    console.log('FizzBuzz')
+  }else if(i % 3 === 0){   //ricchiesta multipli di 3
+    console.log('Fizz')
   }else{    //se non è nessuna delle ricchieste prima allora stampa i numeri non multipli
     console.log(i)
   }
